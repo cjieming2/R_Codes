44b3abda2082fface0a520ed959a58ad96105898
cjieming <cjieming@gmail.com>
2016-04-20 10:55:48 -0700

    added immport_initial_commands.R; rmed immport_data_analyses.R -- basically a renaming of files

--- immport_initial_commands.R
+++ immport_initial_commands.R
@@ -10,6 +10,22 @@ source('/Users/jiemingchen/R_codes/immport_connect.R')
 ## my own library
 source("/Users/jiemingchen/R_codes/jmRlib.R")
 
+## take in the study-id
+study_id <- 'SDY514'
+sdy514_data <- getStudy(study_id)
+
+## access demographics data
+dm_df <- sdy514_data$special_purpose$dm_l$dm_df
+head(dm_df)
+
+## access concomitant medications data
+cm_df <- sdy514_data$interventions$cm_l$cm_df
+head(cm_df)
+
+## get Trial Title from Trial Summary
+ts_df <- sdy514_data$trial_design$ts_l$ts_df
+title <- ts_df$TSVAL[ts_df$TSPARMCD== "TITLE"]
+title
 
 
 ## close tunnel
